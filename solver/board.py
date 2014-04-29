8e422263d00934ba48dfd382cf3c410d10caf6ae
Konrad Strack <konrad@strack.pl>
2014-04-30 01:00:19 +0200

    setting initial values on the board

--- solver/board.py
+++ solver/board.py
@@ -4,8 +4,16 @@ import numpy as np
 
 
 class Board():
-    def __init__(self):
+    def __init__(self, initial_board=None):
         self._board = np.zeros(shape=(9, 9), dtype='i')
+        self.invariants = []
+
+        if initial_board is not None:
+            for r, row in enumerate(initial_board[:9]):
+                for c, col in enumerate(row[:9]):
+                    if col != 0:
+                        self.invariants.append((r, c))
+                        self._board[r, c] = col
 
     def __getitem__(self, position):
         x, y = position
--- tests/unit/test_board.py
+++ tests/unit/test_board.py
@@ -1,9 +1,12 @@
+import numpy
+
 from solver.board import Board
+
 
 __author__ = 'Konrad Strack'
 
 
-class TestBoard(object):
+class TestBoard():
     def test_if_board_has_the_correct_size(self):
         board = Board()
 
@@ -16,3 +19,28 @@ class TestBoard(object):
         board[1, 3] = value
 
         assert board[1, 3] == value
+
+    def test_setting_invariants(self):
+        invariants = [(0, 4), (2, 3), (7, 1)]
+
+        initial_board = numpy.zeros(shape=(9, 9), dtype='i')
+
+        for r, c in invariants:
+            initial_board[r, c] = 7
+
+        board = Board(initial_board)
+        assert set(board.invariants) == set(invariants)
+
+    def test_setting_invariants_on_the_board(self):
+        invariants = [(0, 4), (2, 3), (7, 1)]
+
+        initial_board = numpy.zeros(shape=(9, 9), dtype='i')
+
+        for r, c in invariants:
+            initial_board[r, c] = 7
+
+        board = Board(initial_board)
+
+        for r in range(9):
+            for c in range(9):
+                assert board[r, c] == 0 or (board[r, c] == 7 and (r, c) in invariants)
