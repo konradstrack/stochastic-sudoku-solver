96c9cde8990f0266b8254eebda4a05591b97d7e1
Beata Skiba <bskiba@student.agh.edu.pl>
2014-05-14 09:28:29 +0200

    fixed to use genotype instead of board

--- solver/crossover.py
+++ solver/crossover.py
@@ -1,7 +1,10 @@
+from solver.board import Board
+from solver.genotype import BoardGenotype
+
 class MockCrossover():
     def __init__(self, final_size):
         self.final_size = final_size
 
     def process(self, population):
         for i in xrange(self.final_size - len(population)):
-            population.append(Board())
+            population.append(BoardGenotype(Board()))
