35b1d3b3f6b2a54e68d638d368a7ceae6028a51c
Konrad Strack <konrad@strack.pl>
2014-06-04 01:10:39 +0200

    simple logic for filling intersections

--- solver/logic.py
+++ solver/logic.py
@@ -1,3 +1,5 @@
+from random import Random
+
 __author__ = 'Konrad Strack'
 
 
@@ -10,15 +12,22 @@ def fill(board, uncertainty=1, repeat=4):
 
 
 def fill_intersections(board, uncertainty=1):
+    full = set(range(1, 10))
+    random = Random()
+
     for r, row in enumerate(board):
         for c, value in enumerate(row):
 
             if value == 0:
                 in_row = set(v for v in row)
-                in_col = set(board[i, c] for i in range(board.shape[0]))
+                in_col = set(board[i, c] for i in range(board.shape()[0]))
 
-                print(in_row | in_col)
+                missing = full - (in_row | in_col)
+                if len(missing) <= uncertainty:
+                    board[r, c] = random.choice(list(missing))
+
+    return board
 
 
-def fill_squares(board, uncertaint=1):
+def fill_squares(board, uncertainty=1):
     pass
