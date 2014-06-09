0301e1ecb987c2c93fb19d6c298b5950822e2a03
Beata Skiba <bskiba@student.agh.edu.pl>
2014-06-09 18:25:33 +0200

    Getattr forwarded to board in boardgenotype. Will allow for compact notation.

--- solver/genotype.py
+++ solver/genotype.py
@@ -2,3 +2,8 @@ class BoardGenotype():
     def __init__(self, board):
         self.board = board
         self.fitness = None
+
+    def __getattr__(self,name):
+    	"""Forward methods to board. AttributeError will be raised fo unknown methods. """
+    	attr = getattr(self.board, name)
+    	return attr
