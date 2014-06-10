783e2dcf42b3472f8dd0a1a62bcde1317bd4c0c7
Konrad Strack <konrad@strack.pl>
2014-06-10 23:12:46 +0200

    take squares into account when filling the board

--- solver/logic.py
+++ solver/logic.py
@@ -1,17 +1,20 @@
 from random import Random
 
+import numpy as np
+
+
 __author__ = 'Konrad Strack'
 
 
 def fill(board, uncertainty=1, repeat=4):
     result = board
     for _ in range(repeat):
-        result = fill_intersections(result)
+        result = fill_one(result, uncertainty)
 
     return result
 
 
-def fill_intersections(board, uncertainty=1):
+def fill_one(board, uncertainty=1):
     full = set(range(1, 10))
     random = Random()
 
@@ -21,13 +24,10 @@ def fill_intersections(board, uncertainty=1):
             if value == 0:
                 in_row = set(v for v in row)
                 in_col = set(board[i, c] for i in range(board.shape()[0]))
+                in_square = set(board[ind[0], ind[1]] for ind, n in np.ndenumerate(board.get_square(r // 3, c // 3)))
 
-                missing = full - (in_row | in_col)
-                if len(missing) <= uncertainty:
+                missing = full - (in_row | in_col | in_square)
+                if 0 < len(missing) <= uncertainty:
                     board[r, c] = random.choice(list(missing))
 
     return board
-
-
-def fill_squares(board, uncertainty=1):
-    pass
